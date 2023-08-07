5d70f8060929abae442f531d399d27012a53f983
Doukhaev Israil <doukahevisrail@MacBook-Air-de-Doukahev.local>
2023-08-07 14:43:09 +0200

    update

--- src/app/api/chatGpt/route.ts
+++ src/app/api/chatGpt/route.ts
@@ -4,7 +4,7 @@ import pdfParse from 'pdf-parse';
 
 type bodyType = {
   role: "user",
-  content: string
+  content: string,
 }
 
 const configuration = new Configuration({
@@ -13,27 +13,37 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
-const response = async ( body: bodyType ) => {
+const response = async ( body: bodyType, choosedPrompt?: string ) => {
+
+  let prompt;
+
+  if(choosedPrompt == "lesson") {
+    prompt = `
+            You are an API that embodies all teachers, your goal is to create an exam with the lesson provided to you so that the student can practice.
+            With the lesson provided to you, you need to create the most relevant and useful axem for the student.
+            There will be a level that will be given to you and you will have to adapt the exam to match the level, the possible levels are: easy, normal and difficult.
+            Only the lesson can be accepted and if what you are given is not a lesson, answer "Ce que vous avez fourni n'est pas une leçon, donc je ne peux pas vous donner d'examen".
+            The question must be only answerable with text.
+            Start directly with the questions.
+            After each question, write "endOfQuestion".
+          `
+  } else {
+    prompt = `
+            You are an API who embodies all teachers, you will receive the exam made by someone and your goal is to evaluate it.
+            You will receive the exam result with the questions and you will have to correct the answer to each question if there is an error.
+            Start directly with the corrections.
+            Before and after each question, write "endAndStartOfQuestion".
+            You should give a general comment at the end and give a grade to the exam result.
+            Before the general comment write "startOfComment".
+          `
+  }
+
   const wResponse = await openai.createChatCompletion({
     model: "gpt-3.5-turbo-16k-0613",
     messages: [
       {
         role: "system",
-        content: `
-          You are an API that embodies all teachers, your goal is to create an exam with the lesson provided to you so that the student can practice. You will also receive a result of the exam you provided and you will need to grade the exam.
-          This part is for create the exam: 
-            With the lesson provided to you, you need to create the most relevant and useful axem for the student.
-            There will be a level that will be given to you and you will have to adapt the exam to match the level, the possible levels are: easy, normal and difficult.
-            If what you are given is not a lesson, reply "what you provided is not a lesson, so I cannot give you an exam".
-            The question must be only answerable with text.
-            Start directly with the questions.
-            After each question, write "endOfQuestion".
-
-          This part is for when you receive the exam result:
-            When that start with "Result:" that mean it concern "This part is for when you receive the exam result" part.
-            You will receive the exam result with the questions and you must grade a correction for each answer if there is an error in an answer.
-            You must give at the end a general comment and give the mark of the results for the exam.
-          `
+        content: prompt
       },
       body,
     ]
@@ -50,13 +60,14 @@ export async function POST(request: any) {
       if(typeof body.lesson == "object") {
         const buffer = Buffer.from(await body.lesson.arrayBuffer());
         text = await pdfParse(buffer);
+        text = text.text;
       } else {
         text = body.lesson
       }
 
-      const lesson = text + `\nThe difficulty i want to choose is ${body.difficulty}`
+      const lesson =  `${text} \nThe difficulty i want to choose is ${body.difficulty}`
 
-      const message = await response({role: "user", content: lesson})
+      const message = await response({role: "user", content: lesson}, "lesson")
 
       // return new Response("message")
       return NextResponse.json({ message });
@@ -64,7 +75,6 @@ export async function POST(request: any) {
 
       const responses = await request.json();
 
-      console.log(responses)
       const message = await response({role: "user", content: `Result: ${responses}`})
 
       return NextResponse.json({ message });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,15 @@ import Headers from '@/components/header/Header'
 import Footer from '@/components/footer/Footer'
 import { useGlobalContext } from './context/store';
 
-type ExamType = {
-    role: string,
-    content: string
-}
-
 export default function Home() {
   const {setText} = useGlobalContext();
   const [selectedFile, setSelectedFile] = useState(null);
-  const [exam, setExam] = useState<ExamType | null>(null);
   const [waitAnswer, setWaitAnswer] = useState(true);
   const [chooseDifficulty, setChooseDifficulty] = useState("normal");
   const [lessonText, setLessonText] = useState("");
-  const [allQuestions, setAllQuestions] = useState([]);
+  const [lessonError, setLessonError] = useState(false);
+  const [uploadFile, setUploadFile] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const createExam = async () => {
     try {
@@ -41,65 +37,69 @@ export default function Home() {
       };
 
       const data = await response.json();
-      const questions = data.message.message.content.split("endOfQuestion");
-      questions.pop();
-      setExam(data.message.message);
-      setText(questions)
-      setAllQuestions(questions);
-      window.localStorage.setItem("questions", JSON.stringify([questions, 0]));
+
+      if(data.message.message.content == "Ce que vous avez fourni n'est pas une leçon, donc je ne peux pas vous donner d'examen.") {
+        setLessonError(true)
+      } else {
+        setLoading(true)
+        const questions = data.message.message.content.split("endOfQuestion");
+        questions.pop();
+        setText(questions)
+        window.localStorage.setItem("questions", JSON.stringify([questions, 0]));
+        window.location.href = "/questions"
+      }
     } catch(error:any) {
       console.error(error.message);
-    } 
+    }
   };
 
-  console.log(allQuestions)
-
-  return (
-    <>
-    <Headers />
-    <main className='main_container'>
-      <p className='main_explication_text'>Pour pouvoir créer votre examen vous pouvez mettre la leçon que vous souhaitez dans le champ 
-        ci-dessous manuellement ou par pdf qui ce trouve juste en dessous du champ.</p>
-      <textarea  style={{resize: "none", caretColor: "auto"}} 
-      value={lessonText}
-      className='main_field' 
-      placeholder='Ecrivez ici votre texte...'
-      onChange={(event: {target: {value: string}}) => {
-        setLessonText(event.target.value)
-        if(lessonText.length > 50) {
-          setWaitAnswer(false);
-        } else {
-          setWaitAnswer(true);
-        }
-      }}
-      />
-      <hr className='main_split_trait' />
-      <label htmlFor="file-upload" className="main_pdf-upload">
-        Choisir PDF
-      </label>
-      <select value={chooseDifficulty} onChange={(event: any) => setChooseDifficulty(event.target.value)}>
-        <option>facile</option>
-        <option>normal</option>
-        <option>dificile</option>
-      </select>
-      <input type="file" id="file-upload" accept="application/pdf" 
-        onChange={(event:any) => {
-          setSelectedFile(event.target.files[0])
-          setWaitAnswer(false)
-      }} 
-      style={{display: 'none'}}/>
-
-      {allQuestions ? allQuestions.map((question: string, index: number) => {
-        return (
-          <div key={index}>
-            <p>{question}</p>
-          </div>
-        )
-      }) : null}
-      {/* <a href="/chemin/vers/votre/fichier.pdf" download>Télécharger le PDF</a> */}
-      <button className='main_button_create' onClick={createExam} disabled={waitAnswer}>Créer</button>
-    </main>
-    <Footer />
-    </>
-  )
+  return loading ? 
+      <div className='main_loading'>
+            <h1>Loading...</h1>
+            <p>Vous devrez peut-être attendre un peu.</p>
+      </div>
+    :
+      <>
+        <Headers />
+        <main className='main_container'>
+          <p className='main_explication_text'>Pour pouvoir créer votre examen vous pouvez mettre la leçon que vous souhaitez dans le champ 
+            ci-dessous manuellement ou par pdf qui ce trouve juste en dessous du champ.</p>
+          <textarea  style={{resize: "none", caretColor: "auto"}} 
+          value={lessonText}
+          className='main_field' 
+          placeholder='Ecrivez ici votre texte...'
+          onChange={(event: {target: {value: string}}) => {
+            setLessonText(event.target.value)
+            if(lessonText.length > 50) {
+              setWaitAnswer(false);
+            } else {
+              setWaitAnswer(true);
+            }
+          }}
+          />
+          <hr className='main_split_trait' />
+          <label htmlFor="file-upload" className={`${uploadFile ? "main_pdf_uploaded" : "main_pdf_upload"}`}>
+            Choisir PDF
+          </label>
+          <input type="file" id="file-upload" accept="application/pdf" 
+            onChange={(event:any) => {
+              setSelectedFile(event.target.files[0])
+              setUploadFile(true)
+              setWaitAnswer(false)
+          }} 
+          style={{display: 'none'}}/>
+          <label className='main_choose_difficulty_container'>
+            Vous devez choisir la difficulté de l&#39;examen qui sera créé: {" "}
+            <select value={chooseDifficulty} onChange={(event: any) => setChooseDifficulty(event.target.value)}>
+            <option>facile</option>
+            <option>normal</option>
+            <option>dificile</option>
+          </select>
+          </label>
+          {/* <a href="/chemin/vers/votre/fichier.pdf" download>Télécharger le PDF</a> */}
+          <button className='main_button_create' onClick={createExam} disabled={waitAnswer}>Créer Exam</button>
+          {lessonError ? <p className='main_lesson_error'>Ce que vous avez fourni n&#39;est pas une leçon, donc je ne peux pas vous donner d&#39;examen.</p> : null}
+        </main>
+        <Footer />
+      </>
 }
